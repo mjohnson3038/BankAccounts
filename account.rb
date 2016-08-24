e1ae8d5cca9a392c0e31a84dcbebe72abd963340
Madeleine Johnson <mjohnson3038@gmail.com>
2016-08-24 16:04:34 -0700

    self.all complete and beyond

--- account.rb
+++ account.rb
@@ -1,39 +1,42 @@
+require 'csv'
+
 module Bank
 
   class Account
-    attr_accessor :balance, :id
+    attr_accessor :balance
+    attr_reader :id, :open_date
 
-    def initialize(id)
+    def initialize(id, balance, open_date)
       #initially, I was going to provide the user with a new id, but I wanted them to be able to enter there id and then tell them how much money they had and provide actions for them.
       @id = id
-      @balance = rand(0..1000)
+      @balance = balance.to_f/100
+      @open_date = open_date
     end
 
     def balance
       puts "Your current balance is $#{@balance}."
     end
 
-    def withdrawal
-      puts "Your current balance is $#{@balance}. How much would you like to withdraw?"
-      withdrawal_amount = gets.chomp.to_f
+    def withdraw(amount, message = "Sorry, you do not have sufficient funds for your withdrawal.")
 
-      while @balance < withdrawal_amount
-        puts "Sorry, you do not have sufficient funds for your withdrawal. Please select an amount less than the amount in the account."
-        withdrawal_amount = gets.chomp.to_f
+      if @balance > amount
+        @balance = @balance - amount
+        puts "Please remember to collect your receipt and money before leaving. Your remaining balance is $#{@balance}."
+      else
+        raise Exception.new(message)
       end
-
-      @balance = @balance - withdrawal_amount
-      puts "Please remember to collect your receipt and money before leaving. Your remaining balance is $#{@balance}."
-
     end
 
-    def deposit
-      puts "Please enter the amount of money you are depositing."
-      amount = gets.chomp.to_f
+    def deposit (amount)
       @balance = @balance + amount
       puts "Your new balance is $#{@balance}."
     end
 
+    def self.all
+      customers = []
+      CSV.read('/Users/madeleinejohnson/Ada/project-forks/Bank/BankAccounts/support/accounts.csv','r'). each do |line|
+        customers << self.new(line[0], line[1], line[2])
+      end
+    end
   end
-
 end
--- program.rb
+++ program.rb
@@ -1,30 +1,36 @@
 require_relative 'account'
 
-user = Bank::Account.new(98765)
 
-puts "What can I help you with today? Please select an option below by selecting the letter of the desired action."
-puts "(a) provide balance"
-puts "(b) withdraw cash"
-puts "(c) deposit money into account"
+# The following, commented out code, prints the csv file.
 
-action = gets.chomp
+# Bank::Account.all.each do |line|
+#   puts line
+# end
 
-if action == "a"
-  user.balance
-elsif action == "b"
-  puts "You chose to withdraw cash"
-  user.withdrawal
-elsif action == "c"
-  user.deposit
-else
-  puts "You did not chose a or b"
+puts "Please enter your id before proceeding"
+user_id = gets.chomp
+
+Bank::Account.all.each_with_index do |line, i|
+  if line[0] == user_id
+    puts "What can I help you with today? Please select an option below by selecting the letter of the desired action."
+    puts "(a) provide balance"
+    puts "(b) withdraw cash"
+    puts "(c) deposit money into account"
+    action = gets.chomp
+
+    until action == "quit"
+      if action == "a"
+        Bank::Account.all[i].balance
+      elsif action == "b"
+        puts "You chose to withdraw cash"
+        user.withdrawal
+      elsif action == "c"
+        user.deposit
+      else
+        puts "You did not chose a or b"
+      end
+      puts "Can I help you with anything else? If not please enter 'quit' otherwise select options a,b, or c."
+      action = gets.chomp
+    end
+  end
 end
-
-
-
-
-#
-# puts user.id
-# puts user.balance
-#
-# puts user.balance
